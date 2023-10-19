986327a11de59def84f2e259c563dfdbd8397d84
Anamika Jha <67423289+anamika-1@users.noreply.github.com>
2023-10-19 15:29:45 +0530

    commit

---
+++ src/javaVariable.java
@@ -0,0 +1,9 @@
+public class javaVariable {
+        public static void main(String[] args){
+            int a = 6;
+            System.out.println(a);
+            String name = "Anamika";
+            System.out.println(name);
+        }
+    }
+
