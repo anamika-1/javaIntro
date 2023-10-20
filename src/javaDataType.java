2e5fdd71fc4de535e9b0db3fcbd3bd17bb3f9b13
Anamika Jha <67423289+anamika-1@users.noreply.github.com>
2023-10-20 13:59:45 +0530

    commit

--- src/javaDataType.java
+++ src/javaDataType.java
@@ -1,12 +1,33 @@
 public class javaDataType {
     public static void main(String[] args){
-        // int data type
-        int a = 12;
-        System.out.println(a);
-        float b = 5.6F;
-        System.out.println(b);
-        String name = "Anamika Jha";
-        System.out.println(name);
+        // primitive data type
+        //boolean - can be true / false
+        boolean flag = true;
+        System.out.println(flag);
+
+        //byte - size 1byte / 8bits && range: -128 to 127
+        byte byteValue = 60;
+        System.out.println(byteValue);
+        //short - size 2byte / 16bits && range: -32768 to  32767
+        short shortValue = 1000;
+        System.out.println(shortValue);
+        //int - size 4byte / 32bits && range: -2^31 to 2^31 -1
+        int integerValue = 1268767;
+        System.out.println(integerValue);
+        //long - size 8byte / 64bits && range: -2^64 to 2^64 -1
+        long longValue = 1234569991;
+        System.out.println(longValue);
+
+        //float - size 4byte / 32bits
+        float floatValue = 5.6F;
+        System.out.println(floatValue);
+        //double - size 8byte / 64bits =
+        double doubleValue = 78576.73747;
+        System.out.println(doubleValue);
+
+        //char - size 2byte // 16bits
+        char charValue = 'A';
+        System.out.println(charValue);
 
     }
 }
