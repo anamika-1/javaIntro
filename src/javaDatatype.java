3a1e8c5b5184c9c2b22d307b880eecaabdb3e6e5
Anamika Jha <67423289+anamika-1@users.noreply.github.com>
2023-10-20 14:14:52 +0530

    committhis

---
+++ src/javaDatatype.java
@@ -0,0 +1,31 @@
+import java.sql.SQLOutput;
+
+public class javaDatatype {
+    public static void main(String[] args){
+        boolean booleanValue = true;
+
+        byte byteValue = 21;
+        short shortValue = 123;
+        int intValue = 67347;
+        long longValue = 398498646;
+
+        float floatValue = 6.83f;
+        double doubleValue = 6342823.6787;
+
+        char charValue = 'a';
+
+        System.out.println(booleanValue);
+
+        System.out.println(byteValue);
+        System.out.println(shortValue);
+        System.out.println(intValue);
+        System.out.println(longValue);
+
+        System.out.println(floatValue);
+        System.out.println(doubleValue);
+
+        System.out.println(charValue);
+
+
+    }
+}
